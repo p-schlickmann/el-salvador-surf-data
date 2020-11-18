4df94e1794ef0770dfb147a72aab6d39d79ac9b7
Pedro <windowsxpedro@gmail.com>
2020-11-18 19:23:00 -0300

    Remove: useless TimeoutException import

--- utils/scraper.py
+++ utils/scraper.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import TimeoutException
 
 from utils.conn import DatabaseConnection
 from utils.secrets import EMAIL, PASSWORD
